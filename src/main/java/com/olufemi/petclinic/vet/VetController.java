8ee04cf073298a29c41e1a0b61916f4b8b4c671b
Oladayo Fagbemi <oofagbemi@outlook.com>
2019-10-22 03:00:17 +0200

    added bootstrap and vet index page

--- src/main/java/com/olufemi/petclinic/vet/VetController.java
+++ src/main/java/com/olufemi/petclinic/vet/VetController.java
@@ -1,8 +1,14 @@
 package com.olufemi.petclinic.vet;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+
+  @RequestMapping("/vets")
+  public String vetsList() {
+    return "vets/index";
+  }
 
 }
